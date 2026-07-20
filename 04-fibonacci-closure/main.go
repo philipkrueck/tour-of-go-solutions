76400fa929552e99d21b7bd04f001235d1498f01
Philip Krück <hi@philipkrueck.com>
2026-07-20 20:49:15 +0200

    feat: add exercise 04 solution

---
+++ 04-fibonacci-closure/main.go
@@ -0,0 +1,23 @@
+// Video explanation: https://www.youtube.com/watch?v=doBVd00Qj1g
+
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	first, second := 0, 1
+	return func() int {
+		curr := first
+		first, second = second, first+second
+		return curr
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
