85cd6993e627ab8e0973e129a2e0e874cac8c836
Philip Krück <hi@philipkrueck.com>
2026-07-20 21:13:15 +0200

    feat: add exercise 07 solution

---
+++ 07-readers/main.go
@@ -0,0 +1,18 @@
+// Video solution: https://www.youtube.com/watch?v=YHtBbU4f-6A
+
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+func (r MyReader) Read(b []byte) (int, error) {
+	for i := range b {
+		b[i] = 'A'
+	}
+	return len(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
